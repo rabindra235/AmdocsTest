664edc97d9f07e63cdac6a2e8ae39f522a9d5734
RABINDRASAH <rabindra235@gmail.com>
2016-03-06 23:57:22 +1100

    Car Simulation test

---
+++ CodingTest/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * @author :rabindra sah
+ * created :16/12/2015
+ * contact :rabindrabikramsah@gmail.com
+ */
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+        
+		Simulate gridSimulate =new Simulate();
+		gridSimulate.menu();
+			
+    }
+
+}
